52715a281fbc97aff697963c3d59969e6ec768dd
Vamsi Kallepalli <vkallepalli@uworld.com>
2017-10-16 20:06:02 -0500

    Initial Commit

---
+++ PageViewController/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  PageViewController
+//
+//  Created by Vamsi Kallepalli on 10/16/17.
+//  Copyright © 2017 Vamsi Kallepalli. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
